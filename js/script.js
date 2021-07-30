33e06b73a3f407802aa8b11702e715371d24d8ae
GGO-web <68842054+GGO-web@users.noreply.github.com>
2021-07-31 00:06:19 +0300

    Added Site optimization

--- js/script.js
+++ js/script.js
@@ -146,24 +146,6 @@ for (let productsSlider of productsSliders) {
    });
 }
 
-const searchFormButton = document.querySelector(".search-form__button");
-const searchFormClose = document.querySelector(".search-form__close");
-const searchFormLabel = document.querySelector(".search-form__label");
-
-searchFormButton.addEventListener("click", () => {
-   if (window.innerWidth <= 768) {
-      searchFormButton.type = "button";
-   } else {
-      searchFormButton.type = null;
-   }
-
-   searchFormLabel.classList.toggle("is-showed");
-});
-
-searchFormClose.addEventListener("click", () => {
-   searchFormLabel.classList.remove("is-showed");
-});
-
 "use strict";
 
 const vhCalculate = () => {
